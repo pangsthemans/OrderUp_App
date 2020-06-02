0c8db426e6a9473fc0d44affab5cc4a128d35dcb
pangs <langiwemwale@gmail.com>
2020-06-02 13:17:31 +0200

    just added vector assets for navigation

---
+++ app/src/main/java/com/example/orderup/CustomerMainPage.java
@@ -0,0 +1,14 @@
+package com.example.orderup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_main_page2);
+    }
+}
