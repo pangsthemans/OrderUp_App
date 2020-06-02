9b4786b0321fff6c5ad475cf7cbafc0f5fd4e469
Sipho SK <sipssman@gmail.com>
2020-06-02 13:14:55 +0200

    few error changes

--- app/src/main/java/com/example/orderup/RegistrationPage.java
+++ app/src/main/java/com/example/orderup/RegistrationPage.java
@@ -2,13 +2,31 @@ package com.example.orderup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationPage extends AppCompatActivity {
 
+    private Button Staff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
+        Staff = (Button)findViewById(R.id.StaffBtn);
+
+        Staff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStaffHome();
+            }
+        });
+    }
+
+    public void openStaffHome(){
+        Intent intent = new Intent(this,StaffHome.class);
+        startActivity(intent);
     }
 }
