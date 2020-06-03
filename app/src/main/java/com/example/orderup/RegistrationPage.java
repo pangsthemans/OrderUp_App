072e3b2d49868b97daee364fcf67df20fed3a67a
pangs <langiwemwale@gmail.com>
2020-06-03 22:54:53 +0200

    beginning process for the processing of the registration stuff ,everything until the phpadmin process

--- app/src/main/java/com/example/orderup/RegistrationPage.java
+++ app/src/main/java/com/example/orderup/RegistrationPage.java
@@ -7,6 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
 
 public class RegistrationPage extends AppCompatActivity {
 
@@ -15,7 +24,9 @@ public class RegistrationPage extends AppCompatActivity {
     private Button ConfirmRegis;
     private EditText Username;
     private EditText Password;
+    private ProgressBar loading;
     private EditText reenterPassword;
+    private EditText emailAd;
 
 
 
@@ -25,13 +36,21 @@ public class RegistrationPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
         //for some reason the line below wont allow me to declare this damn button. not sure why
-//        ConfirmRegis=(Button)findViewById(R.id.btn_confirm);
+        ConfirmRegis= findViewById(R.id.btn_confirmRegis);
         Password=findViewById(R.id.txt_password_regis);
+        emailAd=findViewById(R.id.txt_email);
         reenterPassword=findViewById(R.id.txt_reEnterPassword);
         Username=findViewById(R.id.txt_username_regis);
 
-        Customer=(Button)findViewById(R.id.btn_CustomerHome);
-        Staff = (Button)findViewById(R.id.StaffBtn);
+        Customer= findViewById(R.id.btn_CustomerHome);
+        Staff = findViewById(R.id.StaffBtn);
+
+        ConfirmRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         Customer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +70,34 @@ public class RegistrationPage extends AppCompatActivity {
     public void openCustomerHome(){
         Intent intent = new Intent(this,CustomerHome.class);
         startActivity(intent);
+    }
+
+    public void Regist(){
+        loading.setVisibility(View.VISIBLE);
+        ConfirmRegis.setVisibility(View.GONE);
+
+        final String name = this.Username.getText().toString().trim();
+        final String password = this.Password.getText().toString().trim();
+        final String email = this.emailAd.getText().toString().trim();
+
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        })
+//        {
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                return super.getParams();
+//            }
+//        };
     }
 
     public void openStaffHome(){
