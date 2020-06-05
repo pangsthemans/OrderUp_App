1dfe2528cb742de3c9856165055f814414cc8f86
pangs <langiwemwale@gmail.com>
2020-06-05 13:07:05 +0200

    Added the code to login , working on the php , this is pre test phase

--- app/src/main/java/com/example/orderup/MainActivity.java
+++ app/src/main/java/com/example/orderup/MainActivity.java
@@ -7,14 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
     //Using name to login for now but i mean we can always change that
-    private EditText Name;
+    private EditText Email;
     private EditText Password;
-    private Button Login;
+    private Button Login_button;
     private Button Register;
+    private static String URL_LOGIN="https://lamp.ms.wits.ac.za/home/s2039033/login.php";
 
 
     @Override
@@ -22,9 +40,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Connecting all the above declared variables to the ones on the layout
-        Name = (EditText)findViewById(R.id.etName);
+        Email = (EditText)findViewById(R.id.txt_email);
         Password=(EditText)findViewById(R.id.etPassword);
-        Login=(Button)findViewById(R.id.btn_Login);
+        Login_button=(Button)findViewById(R.id.btn_Login);
         Register=(Button)findViewById(R.id.btn_register);
 
 
@@ -35,14 +53,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Login.setOnClickListener(new View.OnClickListener() {
+        Login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String username = Name.getText().toString();
-                final String password = Password.getText().toString();
+                final String email = Email.getText().toString().trim();
+                final String password = Password.getText().toString().trim();
+
+                if (!email.isEmpty() || !password.isEmpty()){
+                    Login(email,password);
+                }else{
+                    Email.setError("Please enter A valid Email");
+                    Password.setError("Please enter A valid Password");
+                }
             }
         } );
     }
+
+    private void Login(final String email, final String password) {
+        Login_button.setVisibility(View.GONE);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success= jsonObject.getString("success");
+                            JSONArray jsonArray=jsonObject.getJSONArray("Login");
+                            if(success.equals("1")){
+                                for(int i=0;i<jsonArray.length();i++){
+                                    JSONObject object=jsonArray.getJSONObject(i);
+                                    String username=object.getString("CUS_USERNAME").trim();
+                                    String email=object.getString("CUS_EMAIL").trim();
+
+                                    Toast.makeText(MainActivity.this,
+                                            "Successful Login. \nYour Name : "
+                                                    +username+"\nYour Email :"+
+                                                    email,Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Login_button.setVisibility(View.VISIBLE);
+                            Toast.makeText(MainActivity.this,"Error "+e.toString(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Login_button.setVisibility(View.VISIBLE);
+                        Toast.makeText(MainActivity.this,"Error "+error.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params=new HashMap<>();
+                params.put("CUS_EMAIL",email);
+                params.put("CUS_PASSWORD",password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
     public void openRegistrationPage(){
         Intent intent = new Intent(this,RegistrationPage.class);
         startActivity(intent);
@@ -52,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    //this function will essentially check if the password matches the one in the database , still need to salt it
+    //this function will essentially check if the password matches the one in the database
     private void validatePassword(String userName, String userPassword){
 
     }
