4c5a608f97c501226311ed33c84469639c975b0d
Sipho SK <sipssman@gmail.com>
2020-06-09 12:40:04 +0200

    Added Customer username and email to the navigation header

--- app/src/main/java/com/example/orderup/CustomerHome.java
+++ app/src/main/java/com/example/orderup/CustomerHome.java
@@ -3,6 +3,7 @@ package com.example.orderup;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -36,6 +37,7 @@ public class CustomerHome extends AppCompatActivity {
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        updateNavHeader();
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -59,5 +61,21 @@ public class CustomerHome extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void updateNavHeader(){
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = headerView.findViewById(R.id.nav_username);
+        TextView navEmail = headerView.findViewById(R.id.nav_email);
+
+        Bundle info = getIntent().getExtras();
+        String name = info.getString("username");
+        String email = info.getString("user_email");
+
+        navUsername.setText(name);
+        navEmail.setText(email);
+
+
     }
 }
