f5875e4bd3449797bf1eba638757b98e627be5cf
pangs <langiwemwale@gmail.com>
2020-06-19 11:41:37 +0200

    trying to change order status but currently bugged . sipho to have a look

--- app/src/main/java/com/example/orderup/ui/gallery/StaffAdapter.java
+++ app/src/main/java/com/example/orderup/ui/gallery/StaffAdapter.java
@@ -1,37 +1,49 @@
 package com.example.orderup.ui.gallery;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.RadioButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.orderup.R;
 import com.example.orderup.StaffHome;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StaffAdapter extends RecyclerView.Adapter<StaffAdapter.ViewHolder> {
 
     private LayoutInflater layoutInflater;
     private ArrayList<String> OrderNumbers;
     private ArrayList<String> OrderCreators;
+    private ArrayList<ViewHolder> testarray;
     private Context mcontext;
     private String selectUpdate;
+    public String url="https://lamp.ms.wits.ac.za/home/s2039033/ProjectLori/changeorderStat.php";
 
     public StaffAdapter(Context context, ArrayList<String> ON, ArrayList<String> OrderC){
         this.layoutInflater = LayoutInflater.from(context);
@@ -59,7 +71,6 @@ public class StaffAdapter extends RecyclerView.Adapter<StaffAdapter.ViewHolder> 
         //Similarly for images and Order creator
         String OCreator = OrderCreators.get(position);
         holder.OrderCreator.setText((OCreator));
-
     }
 
     @Override
@@ -71,25 +82,31 @@ public class StaffAdapter extends RecyclerView.Adapter<StaffAdapter.ViewHolder> 
 
         TextView OrderNumber, OrderCreator;
         ImageView imageView;
+        private Context context;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             //implement onClick to update orders
+            int mypos;
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    openDialog();
+                    int mypos=getAdapterPosition();
+                    String myid= OrderNumbers.get(mypos);
+                    String num=myid.substring(myid.lastIndexOf("#")+1);
+//                    Log.d("TEST",num);
+                    openDialog(num);
                 }
             });
-
+//            this.context=mcontext;
             OrderNumber = itemView.findViewById(R.id.OrderNumber);
             OrderCreator = itemView.findViewById(R.id.OrderCreator);
             imageView = itemView.findViewById(R.id.restaurant);
         }
     }
 
-    public void openDialog(){
-        final String [] updateoptions = {"Pending","Ready", "Collected"};
+    public void openDialog(final String orderid){
+        final String [] updateoptions = {"PENDING","READY", "COLLECTED"};
 
 
         AlertDialog.Builder d = new AlertDialog.Builder(mcontext);
@@ -102,11 +119,61 @@ public class StaffAdapter extends RecyclerView.Adapter<StaffAdapter.ViewHolder> 
         });
         d.setPositiveButton("Update", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(DialogInterface dialog, final int which) {
                 //Send network request here to update the status of the order
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    processJSON(response);
+                                }
+
+                                catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> params= new HashMap<>();
+                        //THis is where i need to post the ID and Status i am changing
+                        //so on this line the update obtions is giving an index of -1 for some reason
+                        params.put("ORDER_ID",orderid);
+                        Log.d("TEST",updateoptions[which]);
+                        params.put("STATUS_CHANGED",updateoptions[which]);
+                        return params;
+                    }
+                };
+
+
+                RequestQueue requestQueue= Volley.newRequestQueue(mcontext);
+                requestQueue.add(stringRequest);
             }
+
         });
         AlertDialog al = d.create();
         al.show();
+    }
+
+    public void processJSON(String json) throws JSONException {
+        JSONArray ja = new JSONArray(json);
+
+        for(int i=0;i<ja.length();i++){
+            JSONObject jo=ja.getJSONObject(i);
+            String success=jo.getString("success");
+            if(success.equals("1")){
+                Log.d("TEST","Successful Change");
+            }
+            else{
+                Log.d("TEST","Something Went Wrong");
+            }
+        }
     }
 }
